daf87d4c0a12302804fd5a85dd5edbdf7cc14b35
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2026-05-24 21:38:25 -0500

    Allow set X/Y to work with counters

--- exporter/src/Events/Actions/SetXYPositionAction.cs
+++ exporter/src/Events/Actions/SetXYPositionAction.cs
@@ -4,7 +4,7 @@ using CTFAK.MMFParser.EXE.Loaders.Events.Parameters;
 
 public class SetXPositionAction : ActionBase
 {
-	public override int[] ObjectType { get; set; } = [2, 3];
+	public override int[] ObjectType { get; set; } = [2, 3, 7];
 	public override int Num { get; set; } = 2;
 
 	public override string Build(EventBase eventBase, ref string nextLabel, ref int orIndex, Dictionary<string, object>? parameters = null, string ifStatement = "if (")
